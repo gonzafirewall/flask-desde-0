7d1e702353cce444844e8bbc263e2aba64cc6623
Gonzalo Martinez <gonzalo@deploshark.com>
2023-01-21 01:34:49 -0300

    parte 2

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
 
+title = "Mi aplicación flask"
+
+frutas_a_comprar = ["banana", "manzana", "naranja"]
+
 @app.get("/")
 def index():
-    return """
-    <form method="POST">
-    <input type="text" name="texto" value="" placeholder="Escribe aqui">
-    <input type="submit">
-    </form>
-    """
-
+    return render_template("index.html", title=title, frutas_a_comprar=frutas_a_comprar)
+    
 @app.post("/")
 def leer_request():
     print(request.form)
